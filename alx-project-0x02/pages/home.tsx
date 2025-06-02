27db8a3677e5e42c5315108a3f7a640bd2a8c457
kaberege <kabgnestor@gmail.com>
2025-06-02 15:47:23 +0200

    added a card component

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Card from '@/components/common/Card'
 
 const home = () => {
     return (
-        <div>
-            <h1 className='text-2xl text-neutral-950 font-bold text-center'>This is the home page</h1>
+        <div className='min-h-screen'>
+            <h1 className='text-2xl text-neutral-950 font-bold text-center mt-4'>This is the home page</h1>
+            <div className='flex gap-3 flex-wrap mt-2'>
+                <Card title='Nice Hotels' content="I like nice hotels where clients are best served." />
+                <Card title='Modern recipes' content="I like Chinese customer care services." />
+            </div>
         </div>
     )
 }
